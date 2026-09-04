b03092fb54110ea2dca853a948f91814fe447db2
ozz-valdo <haniframadhan924@gmail.com>
2026-09-04 16:09:08 +0700

    Add files via upload

---
+++ program_3.1.py
@@ -0,0 +1,32 @@
+#Operasi Aritmatika
+
+a = 10
+b = 3
+
+#Operasi Penjumlahan
+hasil = a + b
+print(a, "+", b, "=", hasil)
+
+#Operasi Pengurangan
+hasil = a - b
+print(a, "-", b, "=", hasil)
+
+#Operasi Perkalian
+hasil = a * b
+print(a, "*", b, "=", hasil)
+
+#Operasi Pembagian
+hasil = a / b
+print(a, "/", b, "=", hasil)
+
+#Operasi Modulus
+hasil = a % b
+print(a, "%", b, "=", hasil)
+
+#Operasi Eksponen
+hasil = a ** b
+print(a, "**", b, "=", hasil)
+
+#Operasi Floor Division
+hasil = a // b
+print(a, "//", b, "=", hasil)
