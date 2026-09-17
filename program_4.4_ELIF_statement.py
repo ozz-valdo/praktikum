a9ceb0fcd9100c43a80814af1403be44edf3a7ba
ozz-valdo <haniframadhan924@gmail.com>
2026-09-17 15:12:08 +0700

    Update program_4.4_ELIF_statement.py

--- program_4.4_ELIF_statement.py
+++ program_4.4_ELIF_statement.py
@@ -14,9 +14,13 @@ nama = input("  masukan nama kamu: ")
 
 if nama == "Hiura Mihate":
     print("Halo my bini!")
+    print("Kamu imut banhet sih")
 elif nama == "Mizuki Akiyama":
     print("Halo my bini!")
     print("kamu cantik bgt sih")
+elif nama == "Nagisa Shiota":
+    print("halo my bini!")
+    print(kamu baik banget')
 else:
     print("skip,gak imut")
     print("ini adalah akhir dari program")
